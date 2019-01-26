8205d9f08c02dff4adaf54164a12c8fb3dc07682
wate123 <junlin0604@gmail.com>
2019-01-26 08:08:41 -0600

    clean

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startHealth = 3;
+    [HideInInspector]public int currentHealth;
+    private bool isDead = false;
+
+	void Awake () {
+        currentHealth = startHealth;
+	}
+	
+    public void TakeDamage(){
+        currentHealth -= 1;
+
+        if (currentHealth <= 0 && !isDead){
+            Dead();
+        }
+    }
+    void Dead(){
+        isDead = true;
+
+        //player dead sound;
+
+        //goto dead animation
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
