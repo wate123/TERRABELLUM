62ec6c3c7a612986455a66287cc24b4335755e0a
wate123 <junlin0604@gmail.com>
2019-01-27 09:52:26 -0600

    progress

---
+++ Assets/Scripts/EnemyWander.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWander : MonoBehaviour {
+    public float wanderSpeedRange;
+    public float alertDistance;
+    private GameObject player;
+    private Transform current;
+
+    // control animation
+    private Animator animator;
+    private AudioClip au;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        current = transform;
+        animator = GetComponent<Animator>();
+        InvokeRepeating("Wander", 0, 0.5f);
+	}
+
+
+    void Wander () {
+        current.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        current.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-wanderSpeedRange, wanderSpeedRange), 0);
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        //current.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-wanderSpeedRange, wanderSpeedRange) * Time.deltaTime, 0);
+	}
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,6 +1,75 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
 public class Inventory : MonoBehaviour
 {
+    //public Image[] itemImages = new Image[numItemSlots];
+    public Dictionary<string, Dictionary<string, ArrayList>> inventory = new Dictionary<string, Dictionary<string, ArrayList>>();
+    private ArrayList fp = new ArrayList();
+    private ArrayList pj = new ArrayList();
+    private ArrayList sh = new ArrayList();
+    public Queue q = new Queue();
 
+	private void Start()
+	{
+        
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.tag == "Enemy"){
+            if (collision.name.Contains("force"))
+            {
+                fp.Add(collision.name);
+                q.Enqueue(collision.name);
+            }
+            else if (collision.name.Contains("projectile"))
+            {
+                pj.Add(collision.name);
+                q.Enqueue(collision.name);
+            }
+            else
+            {
+                sh.Add(collision.name);
+                q.Enqueue(collision.name);
+            }
+            
+        }
+	}
+
+    public void UseAbility() {
+       object first = q.Dequeue();
+        Debug.Log(first);
+    }
+	//public Item[] items = new Item[numItemSlots];
+	//public const int numItemSlots = 4;
+	//public void AddItem(Item itemToAdd)
+	//{
+	//    for (int i = 0; i < items.Length; i++)
+	//    {
+	//        if (items[i] == null)
+	//        {
+	//            items[i] = itemToAdd;
+	//            itemImages[i].sprite = itemToAdd.sprite;
+	//            itemImages[i].enabled = true;
+	//            return;
+	//        }
+	//    }
+	//}
+	//public void RemoveItem(Item itemToRemove)
+	//{
+	//    for (int i = 0; i < items.Length; i++)
+	//    {
+	//        if (items[i] == itemToRemove)
+	//        {
+	//            items[i] = null;
+	//            itemImages[i].sprite = null;
+	//            itemImages[i].enabled = false;
+	//            return;
+	//        }
+	//    }
+	//}
 }
